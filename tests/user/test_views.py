536877cf690566de0d2276bb5c50342a21c5f420
Austin McConnell <austin.s.mcconnell@gmail.com>
2019-02-14 19:03:57 -0600

    Add tests for /notifications endpoint

---
+++ tests/user/test_views.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from flask import url_for
+import maya
+
+
+class TestNotifications:
+
+    def test_get_notifications(self, testapp, authenticated_user):
+        authenticated_user.add_notification(name='panda', data='bear')
+        authenticated_user.save()
+
+        response = testapp.get(url_for('user.notifications'))
+
+        notification = response.json[0]
+        assert notification['name'] == 'panda'
+        assert notification['data'] == 'bear'
+
+    def test_get_notifications_since_yesterday(self, testapp, authenticated_user):
+        authenticated_user.add_notification(name='panda', data='bear')
+        authenticated_user.save()
+
+        yesterday = maya.when('yesterday').datetime().date()
+        response = testapp.get(url_for('user.notifications') + f'?since={yesterday}')
+
+        assert len(response.json) == 1
+
+    def test_get_notifications_since_tomorrow(self, testapp, authenticated_user):
+        authenticated_user.add_notification(name='panda', data='bear')
+        authenticated_user.save()
+
+        tomorrow = maya.when('tomorrow').datetime().date()
+        response = testapp.get(url_for('user.notifications') + f'?since={tomorrow}')
+
+        assert len(response.json) == 0
