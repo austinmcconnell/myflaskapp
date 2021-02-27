edb221ddf636c219f0450706bf15f76c711593e8
Austin McConnell <austin.s.mcconnell@gmail.com>
2021-02-27 12:57:43 -0600

    Fix linting errors

--- app/database.py
+++ app/database.py
@@ -44,7 +44,8 @@ class Model(CRUDMixin, db.Model):
 # From Mike Bayer's "Building the app" talk
 # https://speakerdeck.com/zzzeek/building-the-app
 class SurrogatePK(object):
-    """A mixin that adds a surrogate integer 'primary key' column named ``id`` to any declarative-mapped class."""
+    """A mixin that adds a surrogate integer 'primary key' column
+    named ``id`` to any declarative-mapped class."""
 
     __table_args__ = {'extend_existing': True}
 
