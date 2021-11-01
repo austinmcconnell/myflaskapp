64a8cb5ea1e8ec4d2a49a25a515962b85daa15eb
Austin McConnell <austin.s.mcconnell@gmail.com>
2021-10-31 20:37:21 -0500

    Modify SQLALCHEMY_DATABASE_URI to use postgresql

--- app/settings.py
+++ app/settings.py
@@ -15,6 +15,8 @@ class Config(object):
 
     # Database
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    if isinstance(SQLALCHEMY_DATABASE_URI, str) and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Email
